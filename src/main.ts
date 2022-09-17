32ffdc52e80152d5f9cee54b7ae820b9d46267fb
“keyie” <570588655@qq.com>
2022-09-17 15:18:30 +0800

    add svg-icons

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import './styles/index.scss'
 import { useREM } from '@/utils/flexible'
+import 'virtual:svg-icons-register'
+// 注册svg-icons
 import mLibs from '@/libs'
 
 // useREM()
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { join } from 'path'
+import path, { join } from 'path'
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createSvgIconsPlugin({
+      // 制定需要缓存的目标文件夹
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
+      // 指定symbolId的名称
+      symbolId: 'icon-[name]'
+    })
+  ],
   // 软连接
   resolve: {
     alias: {
