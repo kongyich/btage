54706faa51852eab5fd4475f543a16ecf44617ad
“keyie” <570588655@qq.com>
2022-09-25 15:08:45 +0800

    :sparkles: add search

--- src/libs/search/index.vue
+++ src/libs/search/index.vue
@@ -1,34 +1,89 @@
 <script setup>
-
 </script>
 
 <template>
-  <div class="group relative p-0.5 rounded-xl border-white duration-500 hover:bg-red-100/40">
+  <div class="
+      group
+      relative
+      p-0.5
+      rounded-xl
+      border-white
+      duration-500
+      hover:bg-red-100/40
+    ">
     <div>
       <!-- 搜索图标 -->
       <m-svg-icon name="search" color="#707070" class="w-1.5 h-1.5 absolute translate-y-[-50%] top-[50%] left-2" />
 
       <!-- 输入框 -->
-      <input
-        class="block w-full text-sm h-[44px] pl-4 outline-0 bg-zinc-100 caret-zinc-400 rounded-xl text-zinc-900 tracking-wide font-semibold border duration-500 border-zinc-100 focus:border-red-300 group-hover:bg-white group-hover:border-zinc-200"
-        type="text"
-        placeholder="搜索" />
+      <input class="
+          block
+          w-full
+          text-sm
+          h-[44px]
+          pl-4
+          outline-0
+          bg-zinc-100
+          caret-zinc-400
+          rounded-xl
+          text-zinc-900
+          tracking-wide
+          font-semibold
+          border
+          duration-500
+          border-zinc-100
+          focus:border-red-300
+          group-hover:bg-white group-hover:border-zinc-200
+        " type="text" placeholder="搜索" />
 
       <!-- 删除按钮 -->
-      <m-svg-icon name="input-delete" class="h-1.5 w-1.5 absolute translate-y-[-50%] top-[50%] right-9 cursor-pointer duration-500" />
+      <m-svg-icon name="input-delete" class="
+          h-1.5
+          w-1.5
+          absolute
+          translate-y-[-50%]
+          top-[50%]
+          right-9
+          cursor-pointer
+          duration-500
+        " />
 
       <!-- 分割线 -->
-      <div class="opacity-0 h-1.5 w-[1px] absolute translate-y-[-50%] top-[50%] right-[62px] duration-500 bg-zinc-200"></div>
+      <div class="
+          opacity-0
+          h-1.5
+          w-[1px]
+          absolute
+          translate-y-[-50%]
+          top-[50%]
+          right-[62px]
+          duration-500
+          bg-zinc-200
+          group-hover:opacity-100
+        "></div>
 
       <!-- TODO: 搜索按钮（通用组件） -->
-
-
     </div>
 
     <!-- 下拉区域 -->
 
     <transition name="slide">
-      <div class="max-h-[368px] w-full text-base overflow-auto bg-white absolute z-20 left-0 top-[56px] p-2 rounded border border-zinc-200 duration-200 hover:shadow-2xl">
+      <div class="
+          max-h-[368px]
+          w-full
+          text-base
+          overflow-auto
+          bg-white
+          absolute
+          z-20
+          left-0
+          top-[56px]
+          p-2
+          rounded
+          border border-zinc-200
+          duration-200
+          hover:shadow-2xl
+        ">
         <slot name="dropdown" />
       </div>
     </transition>
@@ -36,17 +91,17 @@
 </template>
 
 <style lang="scss" scoped>
-  .slide-enter-active {
-    transition: all 0.5s;
-  }
-  
-  .slide-leave-active {
-    transition: all 0.5s;
-  }
-  
-  .slide-enter-from,
-  .slide-leave-to {
-    transform: translateY(40px);
-    opacity: 0;
-  }
-  </style>
+.slide-enter-active {
+  transition: all 0.5s;
+}
+
+.slide-leave-active {
+  transition: all 0.5s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateY(40px);
+  opacity: 0;
+}
+</style>
