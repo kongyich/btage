c3a5532347e027350613ae77ee6541699b6e3da6
“keyie” <570588655@qq.com>
2022-10-09 23:14:32 +0800

    :alembic: fix: navigation blinking

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,21 @@
 import { createStore } from 'vuex'
 import getters from './getters'
 import category from './modules/category'
+import createPersistedstate from 'vuex-persistedstate'
 
 const store = createStore({
   getters,
   modules: {
     category
-  }
+  },
+  plugins: [
+    createPersistedstate({
+      // 指定保存到 localStorge 中的key
+      key: 'categorys',
+      // 需要保存的模块
+      paths: ['category']
+    })
+  ]
 })
 
 export default store
--- src/store/modules/category.ts
+++ src/store/modules/category.ts
@@ -1,4 +1,4 @@
-import { ALL_CATEGORY_ITEM } from '@/constants'
+import { ALL_CATEGORY_ITEM, NORMAL_CATEGORYS_DATA } from '@/constants'
 import { getCategory } from '@/api/category'
 // 处理navgation中的数据categorys
 
@@ -6,9 +6,10 @@ export default {
   // 独立作用域
   namespace: true,
   state: {
-    // return {
-    categorys: [ALL_CATEGORY_ITEM]
-    // }
+    // 1. 让categorys具备一个初始化数据
+    // 2. 从服务端获取数据，替换初始化数据
+    // 3. 为了防止初始化数据太老，我们把每次获取到的新数据，都作为下一次的初始化数据
+    categorys: NORMAL_CATEGORYS_DATA
   },
   mutations: {
     setCategorys(state, newCategorys) {
