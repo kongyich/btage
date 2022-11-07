5553c710cf6002eaf70657280dc5e11e238c89a8
“keyie” <570588655@qq.com>
2022-11-07 21:32:03 +0800

    :sparkles: add directive

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -4,6 +4,12 @@ import lazy from './modules/lazy'
 
 export default {
   install(app) {
-    app.directive('lazy', lazy)
+    const directives = import.meta.glob('./modules/*.ts')
+
+    for (const [key, value] of Object.entries(directives)) {
+      const arr = key.split('/')
+      const name = arr[arr.length - 1].replace('.ts', '')
+      app.directive(name, value)
+    }
   }
 }
