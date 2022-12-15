37cfadec6d034e06870c0aab0970d01782eff8b9
“keyie” <570588655@qq.com>
2022-12-15 22:32:50 +0800

    feat:add payment

---
+++ src/views/member/components/payment/mobile-payment/mobile-pay-select.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="py-2 h-[80vh] flex flex-col">
+    <h2 class="text-xl text-zinc-900 dark:text-zinc-200 font-bold mb-2 px-1">
+      选择支付方式
+    </h2>
+    <!-- 支付宝 -->
+    <div
+      class="flex items-center px-2 py-2 border-b border-b-zinc-200 dark:border-b-zinc-600 active:bg-zinc-200 dark:active:bg-zinc-900">
+      <img class="w-4 h-4" src="" alt="" />
+      <p class="text-xl ml-1 text-zinc-800 dark:text-zinc-200">支付宝</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+// import { alipay } from '@/utils/pay'
+
+const props = defineProps({
+  payData: {
+    required: true,
+    type: Object
+  }
+})
+
+// const onAlipay = () => {
+//   alipay(props.payData.title, props.payData.desc)
+// }
+</script>
--- src/views/member/index.vue
+++ src/views/member/index.vue
@@ -10,7 +10,7 @@ import { useStore } from 'vuex'
 import { useRouter } from 'vue-router'
 import { ref, watch } from 'vue'
 import { getVipPayList } from '@/api/pay'
-
+import paymentVue from './components/payment/index.vue'
 /**
  * 列表数据
  */
@@ -64,8 +64,8 @@ const onChangeCurrentPay = (item) => {
         </div>
         <p class="mt-1 text-sm text-zinc-500">{{ currentPayData.desc }}</p>
         <!-- 支付 -->
-        <m-count-down :time="1000 * 60"></m-count-down>
-        <!-- <payment-vue class="mt-4" :payData="currentPayData" /> -->
+        <!-- <m-count-down :time="1000 * 60"></m-count-down> -->
+        <payment-vue class="mt-4" :payData="currentPayData" />
       </div>
     </div>
   </div>
